c5fcaeb54d97f3ba62b2e1a8b6320060682ed341
Anna Kolozsvari <anna.n.kolozsvari@gmail.com>
2016-11-24 08:56:08 +0100

    Ulti and Four Aces implicitly get combined with a Simple, if they were not combined with any other bids.

--- game/Ulti.java
+++ game/Ulti.java
@@ -14,7 +14,7 @@ public class Ulti {
         Player p1 = new Player("Anastasia");
         Player p2 = new Player("Bellamy");
         Player p3 = new Player("Casey");
-        Game game = new Game(p1, p2, p3);
+        Game game = new Game(p1, p2, p3, true);
         
     }
     
